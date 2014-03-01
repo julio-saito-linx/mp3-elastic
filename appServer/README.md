5cb3527f47efd9ff2d5f7556086dcf4134918f5d
Julio Makdisse Saito <saitodisse@gmail.com>
2014-03-01 17:51:07 -0300

    distro now have a simple express server and a README

---
+++ appServer/README.md
@@ -0,0 +1,16 @@
+MP3-Elastic
+===========
+- search music on a simple elastic search database.
+- can play songs over network
+- yo-marionette backbone.js generated
+
+###Instalation
+  * install ElasticSearch
+  * install node.js
+  * import your music with: https://github.com/saitodisse/elasticXbmcMoviesImporter
+    
+    sudo npm install
+
+###Starting server
+    
+    node server.js
--- appServer/server.js
+++ appServer/server.js
@@ -3,18 +3,14 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var async = require('async');
-var hbs = require('express-hbs');
 
 // init express
 var app = express();
 
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
-
-    app.set('view engine', 'handlebars');
-    app.set('views', __dirname + '../app/scripts/views');
-});
+  }
+);
 
 // set logging
 app.use(function(req, res, next){
@@ -43,5 +39,6 @@ app.get('/', function(req, res){
 // start server
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express App started! port:' + app.get('port'));
-});
+  }
+);
 
